06e9dc4eebfb13f5061d8035a02a733ad5f250d0
uuRCnn <ugurcan380@gmail.com>
2024-03-17 21:58:49 +0300

    son commit

--- GameJamPrototip_-main/Assets/MainProje/Script/GoogleScript/Controler/So.cs
+++ GameJamPrototip_-main/Assets/MainProje/Script/GoogleScript/Controler/So.cs
@@ -5,7 +5,7 @@ namespace MainProje.Script.GoogleScript.Controler
   [CreateAssetMenu(fileName = "So", menuName = "ScriptableObjects/So", order = 0)]
   public class So : ScriptableObject
   {
-    public float SoundVoluem;
-    public float MusicVoluem;
+    public float SoundVoluem = 0.1f;
+    public float MusicVoluem = 0.1f;
   }
 }
